15ca57bca3b0012a2195c204382b6a8e558c594b
Seann Wang <seannwang@hotmail.com>
2022-10-24 23:27:03 -0700

    "added jsonreader class methods"

--- src/main/Persisitance/JsonReader.java
+++ src/main/Persisitance/JsonReader.java
@@ -1,4 +1,59 @@
 package Persisitance;
 
+
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import model.Playlist;
+import org.json.*;
+
+
 public class JsonReader {
+    private String source;
+
+    //EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
+
+    // EFFECTS: reads playlist from file and returns it;
+    // throws IOException if an error occurs reading data form file
+    public Playlist read() throws IOException {
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parsePlaylist(jsonObject);
+    }
+
+    //EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try(Stream<String> stream = Files.lines( Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
+    }
+
+    //EFFECTS: parses playlist from JSON object and returns it
+    private Playlist parsePlaylist(JSONObject jsonObject) {
+        String name = jsonObject.getString("name");
+        Playlist playlist = new Playlist();
+        playlist.setName(name);
+        addInfo(playlist, jsonObject);
+        return playlist;
+    }
+
+    // MODIFIES: playlist
+    // EFFECTS: parses info from JSON object and adds them to playlist
+    private void addInfo(Playlist playlist, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("properties");
+        for (Object json : jsonArray) {
+
+        }
+    }
 }
